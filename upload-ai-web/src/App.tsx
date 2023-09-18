f7c8eab14b2b2159415a60fe18437ee4f510735a
GabrielRioo <gabrielpaivario@gmail.com>
2023-09-17 21:01:22 -0300

    Finalizando o projeto com retorno da resposta do prompt

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,13 +1,37 @@
 import { Github, Wand2 } from 'lucide-react'
-import { Button } from "./components/ui/button";
-import { Separator } from './components/ui/separator';
-import { Textarea } from './components/ui/textarea';
-import { Label } from './components/ui/label';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
-import { Slider } from './components/ui/slider';
-import { VideoInputForm } from './components/video-input-form';
+import { Button } from './components/ui/button'
+import { Separator } from './components/ui/separator'
+import { Textarea } from './components/ui/textarea'
+import { Label } from './components/ui/label'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select'
+import { Slider } from './components/ui/slider'
+import { VideoInputForm } from './components/video-input-form'
+import { PromptSelect } from './components/prompt-select'
+import { useState } from 'react'
+import { useCompletion } from 'ai/react'
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+  const { 
+    input, 
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+   } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: { 
+      'Content-Type': 'application/json',
+    },
+  })
+
   return (
     <div className='min-h-screen flex flex-col'>
       <div className="px-6 py-3 flex items-center justify-between border b">
@@ -34,11 +58,14 @@ export function App() {
             <Textarea
               className='resize-none p-5 leading-relaxed' 
               placeholder='Inclua o prompt para IA...'
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
               className='resize-none p-5 leading-relaxed' 
               placeholder='Resultado gerado pela IA...' 
               readOnly
+              value={completion}
             />
           </div>
 
@@ -48,24 +75,15 @@ export function App() {
         </div>
 
         <aside className='w-80 space-y-6'>
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId}/>
 
           <Separator />
 
-          <form className='space-y-6'>
+          <form onSubmit={handleSubmit} className='space-y-6'>
           <div className='space-y-2'>
               <Label>Prompt</Label>
 
-              <Select>
-                <SelectTrigger className=''>
-                  <SelectValue placeholder='Selecione um prompt...'/>
-                </SelectTrigger>
-
-                <SelectContent>
-                  <SelectItem value="title">Titulo do Youtube</SelectItem>
-                  <SelectItem value="description">Descricao do Youtube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput}/>
             </div>
 
             <div className='space-y-2'>
@@ -92,13 +110,15 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
               <span className='block text-xs text-muted-foreground italic leading-relaxed'>Valores mais altos tendem a deixar o resultado mais criativo e com possiveis erros.</span>
             </div>
 
             <Separator />
 
-            <Button type='submit' className='w-full'>
+            <Button disabled={isLoading} type='submit' className='w-full'>
               Executar
               <Wand2 className='w-4 h-4 ml-2' />
             </Button>
